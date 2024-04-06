23f32e58ddf1407deef15cdb5d25baddc14bae1e
tien.xuan.vo <tien.xuan.vo@gmail.com>
2024-04-06 18:36:54 +0700

    refactor(2.0): Rename private method

--- src/Composer/Package/InstallPath/InstallPath.php
+++ src/Composer/Package/InstallPath/InstallPath.php
@@ -13,10 +13,10 @@ class InstallPath implements InstallPathInterface
 
     public function convertToAbsolute(string $relative): string
     {
-        return $this->getParentPath().\DIRECTORY_SEPARATOR.$relative;
+        return $this->getPackageInstallPath().\DIRECTORY_SEPARATOR.$relative;
     }
 
-    private function getParentPath(): string
+    private function getPackageInstallPath(): string
     {
         return InstalledVersions::getInstallPath($this->package->getName());
     }
