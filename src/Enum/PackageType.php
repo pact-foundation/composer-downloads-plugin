278b5527cb27217158cdd7e5341bbcb1ea7dfa07
tienvx <tien.xuan.vo@gmail.com>
2024-04-13 11:18:03 +0700

    refactor(2.0): Remove gzip package type

--- src/Enum/PackageType.php
+++ src/Enum/PackageType.php
@@ -7,6 +7,5 @@ enum PackageType: string
     private const TYPE_PREFIX = 'extra-download:';
 
     case ARCHIVE = self::TYPE_PREFIX.'archive';
-    case GZIP = self::TYPE_PREFIX.'gzip';
     case FILE = self::TYPE_PREFIX.'file';
 }
